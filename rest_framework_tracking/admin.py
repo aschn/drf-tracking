c61bf896ea0380c54fb274bb14c3143eb36fedba
anna <annarschneider@gmail.com>
2015-05-26 12:35:10 -0700

    change fields visible in admin

--- rest_framework_tracking/admin.py
+++ rest_framework_tracking/admin.py
@@ -5,7 +5,9 @@ from .models import APIRequestLog
 class APIRequestLogAdmin(admin.ModelAdmin):
     date_hierarchy = 'requested_at'
     list_display = ('id', 'requested_at', 'response_ms',
-                    'user', 'path', 'query_params', 'remote_addr', 'host')
-    list_filter = ('user', 'path', 'query_params', 'remote_addr', 'host')
+                    'user', 'method',
+                    'path', 'remote_addr', 'host',
+                    'query_params')
+    list_filter = ('user', 'path', 'method', 'remote_addr', 'host')
 
 admin.site.register(APIRequestLog, APIRequestLogAdmin)
