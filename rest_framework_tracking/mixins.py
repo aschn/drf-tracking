0d226667b2165a284f9d16a9b4d2c89ac8ee031e
Mayank Kapoor <mayankkapoor@gmail.com>
2015-11-30 15:57:10 +0530

    override initial instead of initialize_request
    django-rest-framework doesn’t catch exceptions raised in
    initialize_request.

--- rest_framework_tracking/mixins.py
+++ rest_framework_tracking/mixins.py
@@ -1,18 +1,24 @@
 from .models import APIRequestLog
 from django.utils.timezone import now
 
+from rest_framework import exceptions
+
 
 class LoggingMixin(object):
     """Mixin to log requests"""
-    def initialize_request(self, request, *args, **kwargs):
+    def initial(self, request, *args, **kwargs):
         """Set current time on request"""
-        # regular intitialize
-        request = super(LoggingMixin, self).initialize_request(request, *args, **kwargs)
+        # regular initialize
+        super(LoggingMixin, self).initial(request, *args, **kwargs)
 
         # get user
-        if request.user.is_authenticated():
-            user = request.user
-        else:  # AnonymousUser
+        try:
+            if request.user.is_authenticated():
+                user = request.user
+            else:  # AnonymousUser
+                user = None
+        except exceptions.AuthenticationFailed:
+            # AuthenticationFailed exceptions are raised by django-rest-framework in case Token is invalid/expired
             user = None
 
         # get data dict
@@ -33,22 +39,22 @@ class LoggingMixin(object):
             data=data_dict,
         )
 
-        # return
-        return request
-
     def finalize_response(self, request, response, *args, **kwargs):
         # regular finalize response
         response = super(LoggingMixin, self).finalize_response(request, response, *args, **kwargs)
 
-        # compute response time
-        response_timedelta = now() - request.log.requested_at
-        response_ms = int(response_timedelta.total_seconds() * 1000)
+        try:
+            # compute response time
+            response_timedelta = now() - request.log.requested_at
+            response_ms = int(response_timedelta.total_seconds() * 1000)
 
-        # save to log
-        request.log.response = response.rendered_content
-        request.log.status_code = response.status_code
-        request.log.response_ms = response_ms
-        request.log.save()
+            # save to log
+            request.log.response = response.rendered_content
+            request.log.status_code = response.status_code
+            request.log.response_ms = response_ms
+            request.log.save()
+        except AttributeError:
+            pass
 
         # return
         return response
