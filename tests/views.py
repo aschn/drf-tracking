d68e973167a77f2f95def9a612e76353acad335a
anna <annarschneider@gmail.com>
2015-12-21 16:08:13 -0800

    refactor tests and test bug fix

---
+++ tests/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import get_list_or_404
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+from rest_framework_tracking.mixins import LoggingMixin
+from rest_framework_tracking.models import APIRequestLog
+import time
+
+
+class MockNoLoggingView(APIView):
+    def get(self, request):
+        return Response('no logging')
+
+
+class MockLoggingView(LoggingMixin, APIView):
+    def get(self, request):
+        return Response('with logging')
+
+
+class MockSlowLoggingView(LoggingMixin, APIView):
+    def get(self, request):
+        time.sleep(1)
+        return Response('with logging')
+
+
+class MockSessionAuthLoggingView(LoggingMixin, APIView):
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response('with session auth logging')
+
+
+class MockTokenAuthLoggingView(LoggingMixin, APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        return Response('with token auth logging')
+
+
+class MockJSONLoggingView(LoggingMixin, APIView):
+    def get(self, request):
+        return Response({'get': 'response'})
+
+    def post(self, request):
+        return Response({'post': 'response'})
+
+
+class MockValidationErrorLoggingView(LoggingMixin, APIView):
+    def get(self, request):
+        raise serializers.ValidationError('bad input')
+
+
+class Mock404ErrorLoggingView(LoggingMixin, APIView):
+    def get(self, request):
+        empty_qs = APIRequestLog.objects.none()
+        return get_list_or_404(empty_qs)
