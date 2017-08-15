15e403f2530a2ef2c6949836c575a8543a8f65de
Tom Riat <riat.tom@gmail.com>
2017-08-15 09:29:23 +0200

    feat(): don't modify the payload

--- rest_framework_tracking/mixins.py
+++ rest_framework_tracking/mixins.py
@@ -62,7 +62,7 @@ class BaseLoggingMixin(object):
             # DRF logic needs them to be raised by the view for error handling to work correctly.
             self.request.log.data = _clean_data(self.request.data.dict())
         except AttributeError:  # if already a dict, can't dictify
-            self.request.log.data = _clean_data(dict(self.request.data))
+            self.request.log.data = _clean_data(self.request.data)
 
     def handle_exception(self, exc):
         # basic handling
@@ -128,6 +128,7 @@ def _clean_data(data):
     Function based on the "_clean_credentials" function of django
     (django/django/contrib/auth/__init__.py)
     """
+    data = dict(data)
     SENSITIVE_DATA = re.compile('api|token|key|secret|password|signature', re.I)
     CLEANSED_SUBSTITUTE = '********************'
     for key in data:
