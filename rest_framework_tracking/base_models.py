ec3461f9f503eb48d03818ad96eab9ff9fbf8723
Gary Evans <gary@yoyowallet.com>
2019-06-10 14:06:45 +0100

    custom settings to change max_length attrs

--- rest_framework_tracking/base_models.py
+++ rest_framework_tracking/base_models.py
@@ -8,23 +8,25 @@ from .managers import PrefetchUserManager
 @python_2_unicode_compatible
 class BaseAPIRequestLog(models.Model):
     """ Logs Django rest framework API requests """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
-                             blank=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL, null=True,
+        blank=True,
+    )
     requested_at = models.DateTimeField(db_index=True)
     response_ms = models.PositiveIntegerField(default=0)
-    path = models.CharField(max_length=200, db_index=True)
+    path = models.CharField(
+        max_length=getattr(settings, 'DRF_TRACKING_PATH_LENGTH', 200),
+        db_index=True,
+    )
     view = models.CharField(
-        max_length=200,
+        max_length=getattr(settings, 'DRF_TRACKING_VIEW_LENGTH', 200),
         null=True,
         blank=True,
         db_index=True,
     )
-
-    # NOTE: Choosing the longest verb in English language - ought be good
-    #       enough for a while
-    VIEW_METHOD_MAX_LENGTH = len('Floccinaucinihilipilificate')
     view_method = models.CharField(
-        max_length=VIEW_METHOD_MAX_LENGTH,
+        max_length=getattr(settings, 'DRF_TRACKING_VIEW_METHOD_LENGTH', 27),
         null=True,
         blank=True,
         db_index=True,
