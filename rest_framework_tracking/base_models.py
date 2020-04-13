83676c86ac5590a275ad2437a89f9463a1c29f45
proyectoacont <josecandiani@gmail.com>
2020-04-13 13:04:41 -0300

    Update base_models.py
    Ignore Python 2 compatibility APIs when it uses Python 3

--- rest_framework_tracking/base_models.py
+++ rest_framework_tracking/base_models.py
@@ -1,50 +1,94 @@
 from django.db import models
 from django.conf import settings
-from six import python_2_unicode_compatible
-
 from .managers import PrefetchUserManager
 
+import sys
+# Removed private Python 2 compatibility APIs¶
+# While Python 2 support was removed in Django 2.0, some private APIs weren’t removed from Django so that third party
+# apps could continue using them until the Python 2 end-of-life.
+# Since we expect apps to drop Python 2 compatibility when adding support for Django 3.0, we’re removing these APIs
+# at this time.
+# More: https://docs.djangoproject.com/en/3.0/releases/3.0/#removed-private-python-2-compatibility-apis
+# print("sys.version: ", sys.version)
+if sys.version < '3':
+    from django.utils.six import python_2_unicode_compatible
+    @python_2_unicode_compatible
+    class BaseAPIRequestLog(models.Model):
+        """ Logs Django rest framework API requests """
+        user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
+        requested_at = models.DateTimeField(db_index=True)
+        response_ms = models.PositiveIntegerField(default=0)
+        path = models.CharField(max_length=200, db_index=True)
+        view = models.CharField(
+            max_length=200,
+            null=True,
+            blank=True,
+            db_index=True,
+        )
 
-@python_2_unicode_compatible
-class BaseAPIRequestLog(models.Model):
-    """ Logs Django rest framework API requests """
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-    )
-    requested_at = models.DateTimeField(db_index=True)
-    response_ms = models.PositiveIntegerField(default=0)
-    path = models.CharField(
-        max_length=getattr(settings, 'DRF_TRACKING_PATH_LENGTH', 200),
-        db_index=True,
-    )
-    view = models.CharField(
-        max_length=getattr(settings, 'DRF_TRACKING_VIEW_LENGTH', 200),
-        null=True,
-        blank=True,
-        db_index=True,
-    )
-    view_method = models.CharField(
-        max_length=getattr(settings, 'DRF_TRACKING_VIEW_METHOD_LENGTH', 27),
-        null=True,
-        blank=True,
-        db_index=True,
-    )
-    remote_addr = models.GenericIPAddressField()
-    host = models.URLField()
-    method = models.CharField(max_length=10)
-    query_params = models.TextField(null=True, blank=True)
-    data = models.TextField(null=True, blank=True)
-    response = models.TextField(null=True, blank=True)
-    errors = models.TextField(null=True, blank=True)
-    status_code = models.PositiveIntegerField(null=True, blank=True)
-    objects = PrefetchUserManager()
+        # NOTE: Choosing the longest verb in English language - ought be good
+        #       enough for a while
+        VIEW_METHOD_MAX_LENGTH = len('Floccinaucinihilipilificate')
+        view_method = models.CharField(
+            max_length=VIEW_METHOD_MAX_LENGTH,
+            null=True,
+            blank=True,
+            db_index=True,
+        )
+        remote_addr = models.GenericIPAddressField()
+        host = models.URLField()
+        method = models.CharField(max_length=10)
+        query_params = models.TextField(null=True, blank=True)
+        data = models.TextField(null=True, blank=True)
+        response = models.TextField(null=True, blank=True)
+        errors = models.TextField(null=True, blank=True)
+        status_code = models.PositiveIntegerField(null=True, blank=True)
+        objects = PrefetchUserManager()
 
-    class Meta:
-        abstract = True
-        verbose_name = 'API Request Log'
+        class Meta:
+            abstract = True
+            verbose_name = 'API Request Log'
 
-    def __str__(self):
-        return '{} {}'.format(self.method, self.path)
+        def __str__(self):
+            return '{} {}'.format(self.method, self.path)
+else:
+    class BaseAPIRequestLog(models.Model):
+        """ Logs Django rest framework API requests """
+        user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
+        requested_at = models.DateTimeField(db_index=True)
+        response_ms = models.PositiveIntegerField(default=0)
+        path = models.CharField(max_length=200, db_index=True)
+        view = models.CharField(
+            max_length=200,
+            null=True,
+            blank=True,
+            db_index=True,
+        )
+
+        # NOTE: Choosing the longest verb in English language - ought be good
+        #       enough for a while
+        VIEW_METHOD_MAX_LENGTH = len('Floccinaucinihilipilificate')
+        view_method = models.CharField(
+            max_length=VIEW_METHOD_MAX_LENGTH,
+            null=True,
+            blank=True,
+            db_index=True,
+        )
+        remote_addr = models.GenericIPAddressField()
+        host = models.URLField()
+        method = models.CharField(max_length=10)
+        query_params = models.TextField(null=True, blank=True)
+        data = models.TextField(null=True, blank=True)
+        response = models.TextField(null=True, blank=True)
+        errors = models.TextField(null=True, blank=True)
+        status_code = models.PositiveIntegerField(null=True, blank=True)
+        objects = PrefetchUserManager()
+
+        class Meta:
+            abstract = True
+            verbose_name = 'API Request Log'
+
+        def __str__(self):
+            return '{} {}'.format(self.method, self.path)
